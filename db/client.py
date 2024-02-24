a68fcf8eb44ecefc19e7494d1478d5337678e7ca
Bilal Khan <bilal.khan@live.com>
2024-02-24 17:28:17 +1100

    client file remove main

--- db/client.py
+++ db/client.py
@@ -1,37 +1,30 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
-from urllib.parse import quote_plus
-from dotenv import load_dotenv
 import os
-
+from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 MONGO_URI = os.getenv('MONGO_URI')
+
 # Function to create a MongoDB client
 def get_client():
     try:
-        client = MongoClient(MONGO_URI)        
-        print("MongoDB connection successful.")        
+        client = MongoClient(MONGO_URI)
+        print("MongoDB connection successful.")
         return client
-    except ConnectionFailure:
-        print("MongoDB connection failed.")
-        
+    except ConnectionFailure as e:
+        print(f"MongoDB connection failed: {e}")
         return None
 
 # Create a global client to be reused
 client = get_client()
 
-# You can also create a function to get the database object
+# Function to get the database object
 def get_database(db_name):
     if client is not None:
         return client[db_name]
     else:
+        print("No MongoDB client available.")
         return None
 
-def main():
-    # Test the connection
-    client = get_client()
-
-if __name__ == "__main__":
-    main()
